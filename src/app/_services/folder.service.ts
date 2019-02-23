7909710c48cff38104b74ce361ffbd03680078d4
Aldi Alimucaj <aldi.alimucaj@gmail.com>
2019-02-23 22:43:21 +0100

    [FEATRE] folder cut elements

--- src/app/_services/folder.service.ts
+++ src/app/_services/folder.service.ts
@@ -18,19 +18,58 @@ export class FolderService {
    * Copy currently selected items to clipboard
    */
   public copy() {
+    // cannot paste cut and copy at the same time
+    delete this.toCut;
     this.toCopy = Object.assign([], this._selected);
+  }
+
+  /**
+   * Cut currently selected items to clipboard
+   */
+  public cut() {
+    // cannot paste cut and copy at the same time
+    delete this.toCopy;
+    this.toCut = Object.assign([], this._selected);
   }
 
   /**
    * Paste items from clipboard to destination 
    * @param testCaseId destination id
    */
+
   public paste(testCaseId: string) {
+    if (this.toCopy && this.toCopy.length > 0) {
+      return this.pasteCopy(testCaseId);
+    } else if (this.toCut && this.toCut.length > 0) {
+      return this.pasteCut(testCaseId);
+    } else {
+      throw new Error('Nothing to paste');
+    }
+  }
+
+  private pasteCut(testCaseId: string) {
+    const cutValues = this.toCut.map(c => {
+      c.parentId = testCaseId;
+      return Object.assign({ _id: c._id, parentId: testCaseId }, c);
+    });
+    delete this.toCut;
+    return this.testCaseService.updateModel(cutValues);
+  }
+
+  private pasteCopy(testCaseId: string) {
     const copyValues = this.toCopy.map(c => {
       c.parentId = testCaseId;
-      return c;
-    })
+      return Object.assign({}, c);
+    });
+    delete this.toCopy;
     return this.testCaseService.push(copyValues);
+  }
+
+  /**
+   * Delete elements in selection
+   */
+  public delete() {
+    // this.testCaseService.remove(this._selected);
   }
 
   get canCopy() {
